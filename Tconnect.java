7c992ed9cf1933097a4112044a127bac37d86cf5
benhamed2010 <34318148+benhamed2010@users.noreply.github.com>
2017-12-08 20:17:40 +0100

    Add files via upload

--- Tconnect.java
+++ Tconnect.java
@@ -1,26 +1,35 @@
 package ConnectionJavaMysql;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 //import com.mysql.jdbc.Statement; 
 import java.sql.Statement;
+
+
 
 public class Tconnect {
 
 
 	public static void main(String[] args) throws FileNotFoundException  {
 		//CSV
-		String fileName ="audience.csv";
+		String fileName ="monfichier.csv";
 		File file = new File(fileName);
 		
 			Scanner in = new Scanner (file);
 			while (in.hasNextLine()){
 				String line = in.nextLine(); 
+				
+				
 				System.out.println(line+"+++++");
 			}
 			in.close();
@@ -55,12 +64,50 @@ public class Tconnect {
 
 	    }catch(Exception e) {System.out.println(e);}
 	        
-
+	        try {
+	            
+	            String delimiter = ";";
+	            String line = null;
+	            StringTokenizer strToken = null;
+	            BufferedReader bufferReader;
+	            int lineID = 0;
+	            int fieldID = 0;
+	           
+	//Ouvrir le fichier CSV 
+	            bufferReader = new BufferedReader(new FileReader(fileName));
+	//parcourir les lignes du fichier CSV
+	            while ((line = bufferReader.readLine()) != null) {
+	                lineID++;
+	//Parcourir les champs séparés par delimiter
+	                strToken = new StringTokenizer(line, delimiter);
+	                while (strToken.hasMoreTokens()) {
+	                    fieldID++;
+	                    System.out.println("Ligne " + lineID
+	                            + " / champs " + fieldID
+	                            + " : " + strToken.nextToken());
+	                   
+	                   /*int name = fieldID;
+	                   String tranche = strToken.nextToken();
+	            		String quart = strToken.nextToken();
+	            		String chaine = strToken.nextToken();
+	            		String nbindividus = strToken.nextToken();
+	            		System.out.println("tranche:"+fieldID);
+	            		System.out.println("quart:"+quart);
+	            		System.out.println("Chaine:"+chaine);
+	            	System.out.println("nombre individus:"+nbindividus);*/
+	                }
+	                fieldID = 0;
+	            }
+	        } catch (IOException ex) {
+	            Logger.getLogger(Tconnect.class.getName()).log(Level.SEVERE, null, ex);
+	        
 
 	    }
-
-
 	}
+	}
+
+
+	
 //		try{
 //1. Get a connection to database
 //Tconnect myConn= DriverManager.getConnection("jdbc:Mysql://localhost:3306/audience1","","");
